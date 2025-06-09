1cb64694bba17c9d114b0e1e86bb38ddb474bb89
dwarowski <realcarenthusiast@yandex.ru>
2025-06-09 14:06:46 +0300

    create: input validation class

---
+++ BasicInputValidation/class.inputValidation.php
@@ -0,0 +1,20 @@
+<?php
+class InputValidation
+{
+    public static function validate($input, $regex, $type)
+    {
+        $cleanInput = trim($input);
+        $cleanInput = stripslashes($input);
+        $cleanInput = htmlspecialchars($input);
+
+        if (empty($cleanInput)) {
+            return ["valid" => false, "output" => "", "errorMsg" => "$type is empty"];
+        } else if ($cleanInput == "") {
+            return ["valid" => false, "output" => "", "errorMsg" => "Invalid $type"];
+        } else if (!preg_match($regex, $cleanInput)) {
+            return ["valid" => false, "output" => "", "errorMsg" => "Invalid $type"];
+        } else {
+            return ["valid" => true, "output" =>  $cleanInput, "errorMsg" => ""];
+        }
+    }
+}
