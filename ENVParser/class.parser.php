4da4931c2c43eda203c8dc52cb9bde4a77d041e0
dwarowski <realcarenthusiast@yandex.ru>
2025-06-11 15:48:17 +0300

    refactor: documented

--- ENVParser/class.parser.php
+++ ENVParser/class.parser.php
@@ -1,23 +1,45 @@
 <?php
+/**
+ * Simple parser for env file 
+ */
 class Parser
 {
-    public static function parseEnv($path)
+    /**
+     * Parse through env file using absolute path
+     * @param string $path
+     * @throws \Exception
+     * @return string[]
+     */
+    public static function parseEnv(string $path)
     {
+        # Clear whitespaces
         $envPath = trim($path);
+
+        # Check if file exsits
         if (!file_exists($envPath)) {
             throw new Exception("Env file not found");
         }
 
+        # Open file and write lines in variable
         $env = file($envPath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
+        # Read line by line
         foreach ($env as $line) {
+
+            # Comments skip
             if (strpos(trim($line), "#") === 0) {
-                continue; # Comments skip
+                continue;
             }
+
+            # Separate name and values
             [$name, $value] = explode("=", $line, 2);
+
+            # Clear whitespaces and quotes
             $name = trim($name);
             $value = trim($value);
             $value = trim($value, "\"");
+
+            # Fill array with name and value
             $envArray[$name] = $value;
         }
         return $envArray;
