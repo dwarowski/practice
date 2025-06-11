f95b0108dea91700edb5d43eb3192f3757a6f1c6
dwarowski <realcarenthusiast@yandex.ru>
2025-06-11 16:56:39 +0300

    refactor: validate function documented

--- BasicInputValidation/class.inputValidation.php
+++ BasicInputValidation/class.inputValidation.php
@@ -4,12 +4,21 @@
  */
 class InputValidation
 {
-    public static function validate($input, $regex, $type)
+    /**
+     * Summary of validate
+     * @param string $input
+     * @param string $regex
+     * @param string $type
+     * @return array{errorMsg: string, output: string, valid: bool}
+     */
+    public static function validate(string $input, string $regex, string $type)
     {
+        # Clean whitespaces and html symbols
         $cleanInput = trim($input);
         $cleanInput = stripslashes($input);
         $cleanInput = htmlspecialchars($input);
 
+        # Validations
         if (empty($cleanInput)) {
             return ["valid" => false, "output" => "", "errorMsg" => "$type is empty"];
         } else if ($cleanInput == "") {
