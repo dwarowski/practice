269789a2b444626e087e6ffa77fcca2629a39214
dwarowski <realcarenthusiast@yandex.ru>
2025-06-11 15:33:54 +0300

    add: file not found exception

--- ENVParser/class.parser.php
+++ ENVParser/class.parser.php
@@ -4,7 +4,12 @@ class Parser
     public static function parseEnv($path)
     {
         $envPath = trim($path);
+        if (!file_exists($envPath)) {
+            throw new Exception("Env file not found");
+        }
+
         $env = file($envPath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
         foreach ($env as $line) {
             if (strpos(trim($line), "#") === 0) {
                 continue; # Comments skip
